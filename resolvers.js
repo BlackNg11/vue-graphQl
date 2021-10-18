e8f4c56db6607ed6b6135972633c475c23607713
Black Nguyen <blackng10.5.1997@gmail.com>
2021-10-18 17:52:39 +0700

    'update'

--- resolvers.js
+++ resolvers.js
@@ -1,14 +1,11 @@
 module.exports = {
   Query: {
     getPosts: async (_, args, { Post }) => {
-      const posts = await Post.find({})
-        .sort({
-          createdDate: "desc",
-        })
-        .populate({
-          path: "createdBy",
-          model: "User",
-        });
+      const posts = await Post.find({}).sort({ createdDate: "desc" }).populate({
+        path: "createdBy",
+        model: "User",
+      });
+      return posts;
     },
   },
   Mutation: {
@@ -24,24 +21,18 @@ module.exports = {
         description,
         createdBy: creatorId,
       }).save();
-
       return newPost;
     },
     signupUser: async (_, { username, email, password }, { User }) => {
-      const user = await User.findOne({
-        username,
-      });
-
+      const user = await User.findOne({ username });
       if (user) {
         throw new Error("User already exists");
       }
-
       const newUser = await new User({
         username,
         email,
         password,
       }).save();
-
       return newUser;
     },
   },
