6e539b37f6ee22cb4c63427348c194c1bcf80565
Black Nguyen <blackng10.5.1997@gmail.com>
2021-10-19 11:04:13 +0700

    'update'

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,12 @@
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+
+const createToken = (user, secret, expiresIn) => {
+  const { username, email } = user;
+
+  return jwt.sign({ username, email }, secret, { expiresIn });
+};
+
 module.exports = {
   Query: {
     getPosts: async (_, args, { Post }) => {
@@ -22,6 +31,22 @@ module.exports = {
         createdBy: creatorId,
       }).save();
       return newPost;
+    },
+    signinUser: async (_, { username, password }, { User }) => {
+      const user = await User.findOne({ username });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      const isValidPassword = await bcrypt.compare(password, user.password);
+
+      console.log(password);
+      console.log(user.password);
+
+      if (!isValidPassword) {
+        throw new Error("Invalid Password");
+      }
+
+      return { token: createToken(user, "Test", "1hr") };
     },
     signupUser: async (_, { username, email, password }, { User }) => {
       const user = await User.findOne({ username });
