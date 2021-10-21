44d349d70f31a13715dbedf276d5ece7971b05e6
Black Nguyen <blackng10.5.1997@gmail.com>
2021-10-21 17:12:30 +0700

    'update'

--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -30,7 +30,7 @@
 
                 <v-list-tile-content>
                   <v-list-tile-title class="text--primary">{{post.createdBy.username}}</v-list-tile-title>
-                  <v-list-tile-subtitle class="font-weight-thin">Added {{post.createdDate}}</v-list-tile-subtitle>
+                  <v-list-tile-sub-title class="font-weight-thin">Added {{formatCreatedDate(post.createdDate)}}</v-list-tile-sub-title>
                 </v-list-tile-content>
 
                 <v-list-tile-action>
@@ -59,6 +59,7 @@
 </template>
 
 <script>
+import moment from "moment";
 import { INFINITE_SCROLL_POSTS } from "../../queries";
 
 const pageSize = 2;
@@ -68,7 +69,7 @@ export default {
   data() {
     return {
       pageNum: 1,
-      showMoreEnabled: true,
+      // showMoreEnabled: true,
       showPostCreator: false,
     };
   },
@@ -81,9 +82,17 @@ export default {
       },
     },
   },
+  computed: {
+    showMoreEnabled() {
+      return this.infiniteScrollPosts && this.infiniteScrollPosts.hasMore;
+    },
+  },
   methods: {
     goToPost(postId) {
       this.$router.push(`/posts/${postId}`);
+    },
+    formatCreatedDate(date) {
+      return moment(new Date(date)).format("ll");
     },
     showMorePosts() {
       this.pageNum += 1;
@@ -100,7 +109,7 @@ export default {
 
           const newPosts = fetchMoreResult.infiniteScrollPosts.posts;
           const hasMore = fetchMoreResult.infiniteScrollPosts.hasMore;
-          this.showMoreEnabled = hasMore;
+          // this.showMoreEnabled = hasMore;
 
           return {
             infiniteScrollPosts: {
